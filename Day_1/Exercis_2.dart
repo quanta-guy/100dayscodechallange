8536117ef71544188f27821d29d1e23523814cfd
Vishnukanth S <vkanthishnu@gmail.com>
2022-06-27 15:37:46 +0530

    Day_1

---
+++ Day_1/Exercis_2.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+  print("Please enter a number : ");
+  var x = int.parse(stdin.readLineSync()!);
+  if (x % 2 != 0) {
+    print("Entered Number is odd");
+  } else {
+    print("Entered number is even");
+  }
+}
---
+++ Day_1/Exercise_1.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  var x, y;
+  print("Enter your name : ");
+  x = stdin.readLineSync();
+  print("Enter your age : ");
+  y = int.parse(stdin.readLineSync()!);
+  print("Dear $x you have ${100 - y} yrs to become 100");
+}
